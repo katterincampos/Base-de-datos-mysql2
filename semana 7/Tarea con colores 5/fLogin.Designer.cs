7caa92ed9ae0cf2966f91b1599ea86e09cf54062
JUAN ALFREDO MARTINEZ MOLINA <USIS001821@ugb.edu.sv>
2021-09-18 20:45:34 -0600

    Login

--- semana 7/Tarea con colores 5/fLogin.Designer.cs
+++ semana 7/Tarea con colores 5/fLogin.Designer.cs
@@ -43,7 +43,7 @@ namespace Tarea_con_colores_5
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.BackColor = System.Drawing.SystemColors.Menu;
             this.label1.Font = new System.Drawing.Font("Calisto MT", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(49, 207);
             this.label1.Name = "label1";
@@ -62,7 +62,7 @@ namespace Tarea_con_colores_5
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.BackColor = System.Drawing.SystemColors.Menu;
             this.label2.Font = new System.Drawing.Font("Calisto MT", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(88, 267);
             this.label2.Name = "label2";
@@ -106,7 +106,7 @@ namespace Tarea_con_colores_5
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.SystemColors.Control;
+            this.label3.BackColor = System.Drawing.SystemColors.Menu;
             this.label3.Font = new System.Drawing.Font("Lucida Calligraphy", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(109, 9);
             this.label3.Name = "label3";
