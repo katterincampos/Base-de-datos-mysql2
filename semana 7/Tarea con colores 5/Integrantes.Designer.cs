8f209cc844bfc368a52b13bc7dc642487468b8ae
Juan-Carlos-Mejía <usis002221@ugb.edu.sv>
2021-09-27 17:42:37 -0600

    colores

--- semana 7/Tarea con colores 5/Integrantes.Designer.cs
+++ semana 7/Tarea con colores 5/Integrantes.Designer.cs
@@ -39,11 +39,11 @@ namespace Tarea_con_colores_5
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.Color.Aqua;
-            this.button1.Location = new System.Drawing.Point(35, 71);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.BackColor = System.Drawing.Color.Moccasin;
+            this.button1.Location = new System.Drawing.Point(47, 87);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(150, 65);
+            this.button1.Size = new System.Drawing.Size(200, 80);
             this.button1.TabIndex = 0;
             this.button1.Text = "Jose Salinas";
             this.button1.UseVisualStyleBackColor = false;
@@ -51,11 +51,11 @@ namespace Tarea_con_colores_5
             // 
             // button2
             // 
-            this.button2.BackColor = System.Drawing.Color.Aqua;
-            this.button2.Location = new System.Drawing.Point(257, 71);
-            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.BackColor = System.Drawing.Color.Moccasin;
+            this.button2.Location = new System.Drawing.Point(343, 87);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(150, 65);
+            this.button2.Size = new System.Drawing.Size(200, 80);
             this.button2.TabIndex = 1;
             this.button2.Text = "Karla Castellanos";
             this.button2.UseVisualStyleBackColor = false;
@@ -63,11 +63,11 @@ namespace Tarea_con_colores_5
             // 
             // button3
             // 
-            this.button3.BackColor = System.Drawing.Color.Aqua;
-            this.button3.Location = new System.Drawing.Point(465, 71);
-            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.BackColor = System.Drawing.Color.Moccasin;
+            this.button3.Location = new System.Drawing.Point(620, 87);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(150, 65);
+            this.button3.Size = new System.Drawing.Size(200, 80);
             this.button3.TabIndex = 2;
             this.button3.Text = "Juan Mejía";
             this.button3.UseVisualStyleBackColor = false;
@@ -75,11 +75,11 @@ namespace Tarea_con_colores_5
             // 
             // button4
             // 
-            this.button4.BackColor = System.Drawing.Color.Aqua;
-            this.button4.Location = new System.Drawing.Point(35, 259);
-            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.BackColor = System.Drawing.Color.Moccasin;
+            this.button4.Location = new System.Drawing.Point(47, 319);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(150, 65);
+            this.button4.Size = new System.Drawing.Size(200, 80);
             this.button4.TabIndex = 3;
             this.button4.Text = "Juan Martínez";
             this.button4.UseVisualStyleBackColor = false;
@@ -87,11 +87,11 @@ namespace Tarea_con_colores_5
             // 
             // button5
             // 
-            this.button5.BackColor = System.Drawing.Color.Aqua;
-            this.button5.Location = new System.Drawing.Point(257, 259);
-            this.button5.Margin = new System.Windows.Forms.Padding(2);
+            this.button5.BackColor = System.Drawing.Color.Moccasin;
+            this.button5.Location = new System.Drawing.Point(343, 319);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(150, 65);
+            this.button5.Size = new System.Drawing.Size(200, 80);
             this.button5.TabIndex = 4;
             this.button5.Text = "Katterin Campos";
             this.button5.UseVisualStyleBackColor = false;
@@ -99,11 +99,11 @@ namespace Tarea_con_colores_5
             // 
             // button6
             // 
-            this.button6.BackColor = System.Drawing.Color.Aqua;
-            this.button6.Location = new System.Drawing.Point(465, 259);
-            this.button6.Margin = new System.Windows.Forms.Padding(2);
+            this.button6.BackColor = System.Drawing.Color.Moccasin;
+            this.button6.Location = new System.Drawing.Point(620, 319);
+            this.button6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(150, 65);
+            this.button6.Size = new System.Drawing.Size(200, 80);
             this.button6.TabIndex = 5;
             this.button6.Text = "Emerson Mejia";
             this.button6.UseVisualStyleBackColor = false;
@@ -111,17 +111,17 @@ namespace Tarea_con_colores_5
             // 
             // Integrantes
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(668, 392);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(891, 482);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Integrantes";
             this.Text = "Integrantes";
             this.Load += new System.EventHandler(this.Integrantes_Load);
